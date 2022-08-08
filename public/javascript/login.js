cb1c807bb9a0943d80b744a7758a859f87d59b1d
Kilimba <dyaul003@umn.edu>
2022-08-08 01:10:33 -0500

    checking heroku

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -4,7 +4,6 @@ async function loginFormHandler(event) {
     const username = document.querySelector('#username-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
   
-    if (username && password) {
       const response = await fetch('/api/users/login', {
         method: 'post',
         body: JSON.stringify({
@@ -20,7 +19,7 @@ async function loginFormHandler(event) {
         alert(response.statusText);
       }
     }
-  }
+  
   
   async function signupFormHandler(event) {
     event.preventDefault();
@@ -28,7 +27,6 @@ async function loginFormHandler(event) {
     const username = document.querySelector('#username-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
   
-    if (username && password) {
       const response = await fetch('/api/users', {
         method: 'post',
         body: JSON.stringify({
@@ -43,7 +41,7 @@ async function loginFormHandler(event) {
       } else {
         alert(response.statusText);
       }
-    }
+    
   }
   
   document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
